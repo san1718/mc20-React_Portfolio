51f447b1ecf5fb95f058f724ffa507f48c4487ea
Sung Min An <an.sungmin17@gmail.com>
2025-04-06 20:30:13 -0600

    update pages

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -44,6 +44,16 @@ function NavTabs({ currentPage, handlePageChange }) {
           Contact
         </a>
       </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
     </ul>
   );
 }

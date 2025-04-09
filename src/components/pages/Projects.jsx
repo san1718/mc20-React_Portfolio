115fe6a73c921ddae5c9cc959f80a5078d98b989
Sung Min An <an.sungmin17@gmail.com>
2025-04-09 12:00:55 -0600

    prj prog

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,10 +1,10 @@
 export default function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
-      <p>
-        These are some of the projects I have been working on.
-      </p>
-    </div>
+    <section>
+      <div>
+        <h1>Projects</h1>
+        <p>These are some of the projects I have been working on.</p>
+      </div>
+    </section>
   );
 }

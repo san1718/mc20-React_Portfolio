418e8779b2bc041cca1bf52e482cf96cca39eb02
Sung Min An <an.sungmin17@gmail.com>
2025-04-14 23:37:22 -0600

    update format

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,9 +1,9 @@
 const Resume = () => {
-    return (
-        <section className="resume p-6 max-w-2xl mx-auto">
-            <h2 className="text-3xl font-bold mb-4">Resume</h2>
-        </section>
-    );
+  return (
+    <section className="resume p-6 max-w-2xl mx-auto">
+      <h2 className="text-3xl font-bold mb-4">Resume</h2>
+    </section>
+  );
 };
 
 export default Resume;

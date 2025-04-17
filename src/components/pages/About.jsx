0a1ce9ab073bea74b401789d9e4ce43f83c5f460
Sung Min An <an.sungmin17@gmail.com>
2025-04-16 23:14:09 -0600

    about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,21 +6,23 @@ const About = () => {
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
       <p className="mb-4">
         {/* Introduction */}
-        Hello! I'm <strong>Sung Min An</strong>, a passionate and dedicated developer with a strong
-        foundation in full-stack web development. My journey in coding has been marked by continuous
-        learning and hands-on experience, leading to the creation of diverse projects that showcase
-        my skills and growth in the field.
+        Hello! I'm <strong>Sung Min An</strong>, a passionate and dedicated
+        developer with a strong foundation in full-stack web development. My
+        journey in coding has been marked by continuous learning and hands-on
+        experience, leading to the creation of diverse projects that showcase my
+        skills and growth in the field.
       </p>
 
-      <h3 className="text-2xl font-semibold mt-6 mb-2">My Development Journey</h3>
+      <h3 className="text-2xl font-semibold mt-6 mb-2">
+        My Development Journey
+      </h3>
       <p className="mb-4">
         {/* Flexibility on projects */}
-        From the outset, I've been committed to building applications that are both functional and
-        user-friendly. My projects reflect a progression from fundamental web technologies to more
-        complex frameworks and APIs, illustrating my adaptability and eagerness to embrace new
-        challenges.
+        From the outset, I've been committed to building applications that are
+        both functional and user-friendly. My projects reflect a progression
+        from fundamental web technologies to more complex frameworks and APIs,
+        illustrating my adaptability and eagerness to embrace new challenges.
       </p>
-
     </section>
   );
 };

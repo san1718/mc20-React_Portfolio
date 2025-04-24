10bddadd90f488e6ccb07513ba3e17c27eac9de2
Sung Min An <an.sungmin17@gmail.com>
2025-04-24 15:28:00 -0600

    page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,6 +2,7 @@ const Resume = () => {
   return (
     <section className="resume p-6 max-w-2xl mx-auto">
       <h2 className="text-3xl font-bold mb-4">Resume</h2>
+      <p></p>
     </section>
   );
 };
